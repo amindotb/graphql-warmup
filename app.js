175df0938d9e512cd2492cbdef196a35501b5355
Amin Bagheri <amindotb@gmail.com>
2020-08-17 12:50:55 +0430

    init ...

--- app.js
+++ app.js
@@ -13,14 +13,32 @@ const app = express()
 const movies = [
     {
         id:1,
-        name:"Start trak",
-        decription:"A good movie",
+        name:"Inception",
+        story:"A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O.",
         producerId:1
     },
     {
         id:2,
-        name:"Night at sky",
-        decription:"Some other movie",
+        name:"Interstellar",
+        story:"A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
+        producerId:2
+    },
+    {
+        id:3,
+        name:"Avengers: Endgame",
+        story:"After the devastating events of Avengers: Infinity War (2018), the universe is in ruins.",
+        producerId:1
+    },
+    {
+        id:4,
+        name:"Forrest Gump",
+        story:"The presidencies of Kennedy and Johnson, the events of Vietnam, Watergate and other historical events unfold through the perspective of an Alabama man with an IQ of 75, whose only desire is to be reunited with his childhood sweetheart.",
+        producerId:1
+    },
+    {
+        id:5,
+        name:"Alita: Battle Angel",
+        story:"A deactivated cyborg is revived, but cannot remember anything of her past life and goes on a quest to find out who she is.",
         producerId:2
     }
 ];
@@ -32,44 +50,9 @@ const producers = [
     },
     {
         id:2,
-        name:"Disney"
+        name:"Twentieth Century Fox"
     },
 ];
 
-const MovieType = new GraphQLObjectType({
-    name:"Movie",
-    description:"Represents  a movie.",
-    fields: ()=>({
-        id: {type:GraphQLNonNull(GraphQLInt)},
-        name: {type:GraphQLNonNull(GraphQLString)},
-        decription: {type:GraphQLNonNull(GraphQLString)},
-        producerId: { type: GraphQLNonNull(GraphQLInt) }             
-    })
-});
-
-const RootQueryType = new GraphQLObjectType({
-    name: 'Query',
-    description: 'Root Query',
-    fields: () => ({
-      movie: {
-        type: MovieType,
-        description: 'A Single Movie',
-        args: {
-          id: { type: GraphQLInt }
-        },
-        resolve: (parent, args) => movies.find(movie => movie.id === args.id)
-      },
-     
-    })
-  })
-
-const schema = new GraphQLSchema({
-    query: RootQueryType,
-});  
-
-app.use('/graphql', graphqlHTTP({
-    schema: schema,
-    graphiql: true
-}));
 
 app.listen(5000, () => console.log('Server Running'))
